8b9c71af66516376c8678848a38dc0bafd721c0a
salik617 <salikrahman1@gmail.com>
2023-09-12 11:23:06 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # get 'forecasts/show'
+
+  # Defines the root path route ("/")
+  root "forecasts#show"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
